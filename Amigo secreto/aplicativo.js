c88634be09d13852720b925ad9f6e25a5ea49203
Éverton Simplício <141969632+EvertonSimplicio1710@users.noreply.github.com>
2025-01-28 18:49:11 -0300

    Add files via upload

---
+++ Amigo secreto/aplicativo.js
@@ -0,0 +1,10 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []; // Declara um array vazio
+
+// Adicionando nomes ao array
+amigos.push("Ana");
+amigos.push("Carlos");
+amigos.push("João");
+
+console.log(amigos); // ["Ana", "Carlos", "João"]
